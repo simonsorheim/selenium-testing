8f50d72b4cdd8d1c16d69a5ce7ad076c6b13014d
Simon Sørheim <simon.sorheim@gmail.com>
2022-10-20 23:43:58 +0200

    Basic setup.

---
+++ src/main/java/com/github/simonsorheim/App.java
@@ -0,0 +1,8 @@
+package com.github.simonsorheim;
+
+public class App {
+
+	public static void main(String[] args) {
+		// Do nothing
+	}
+}
---
+++ src/test/java/com/github/simonsorheim/SeleniumTest.java
@@ -0,0 +1,16 @@
+package com.github.simonsorheim;
+
+import org.junit.Test;
+
+/**
+ * SeleniumTest
+ * Created 2022-10-20
+ *
+ * @author simon
+ */
+public class SeleniumTest {
+
+	@Test
+	public void seleniumTest() {
+	}
+}
